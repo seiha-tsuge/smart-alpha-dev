4f00b34a0c75fa852f88a6a6f4c30a519cd83d83
seiha-tsuge <seiha.tsuge@gmail.com>
2024-01-07 19:31:00 +0900

    feat: contextにidTokenを追加

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -159,6 +159,7 @@ export const protectedProcedure = t.procedure.use(async ({ ctx, next }) => {
     ctx: {
       // infers the `session` as non-nullable
       session: { ...ctx.session, user: ctx.session.user },
+      idToken: JQuantsDataEntity.getAllProperties().idToken,
     },
   });
 });
